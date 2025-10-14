5cdf4da3c0c85f277334e8f626a690a5ae17cf17
Igor Rodrigues <igorar2006@gmail.com>
2025-10-14 11:09:19 -0300

    feat: entidades.hpp e entidades.cpp

--- entidades.cpp
+++ entidades.cpp
@@ -1,4 +1,165 @@
 #include <iostream>
 #include "entidades.hpp"
+#include <string>
 
 using namespace std;
+
+// ---------  PESSOA  ------------
+
+void Pessoa::setNome(Nome novoNome) {
+     this->nome = novoNome;
+}
+
+Nome Pessoa::getNome() const {
+     return this->nome;
+}
+
+void Pessoa::setEmail(Email novoEmail) {
+     this->email = novoEmail;
+}
+
+Email Pessoa::getEmail() const {
+     return this->email;
+}
+
+// ---------- GERENTE  ------------
+
+void Gerente::setSenha(Senha novaSenha) {
+     this->senha = novaSenha;
+}
+
+Senha Gerente::getSenha() const {
+     return this->senha;
+}
+
+void Gerente::setRamal(Ramal novoRamal) {
+     this->ramal = novoRamal;
+}
+
+Ramal Gerente::getRamal() const {
+     return this->ramal;
+}
+
+
+// ---------- HOSPEDE ---------- 
+
+void Hospede::setCartao(Cartao novoCartao) {
+     this->cartao = novoCartao;
+}
+
+Cartao Hospede::getCartao() const {
+     return this->cartao;
+}
+
+void Hospede::setEndereco(Endereco novoEndereco) {
+     this->endereco = novoEndereco;
+}
+
+Endereco Hospede::getEndereco() const {
+     return this->endereco;
+}
+
+
+// ----------  RESERVA  ---------- 
+
+void Reserva::setChegada(Data novaChegada) {
+     this->chegada = novaChegada;
+}
+
+Data Reserva::getChegada() const {
+     return this->chegada;
+}
+
+void Reserva::setPartida(Data novaPartida) {
+     this->partida = novaPartida;
+}
+
+Data Reserva::getPartida() const {
+     return this->partida;
+}
+
+void Reserva::setValor(Dinheiro novoValor) {
+     this->valor = novoValor;
+}
+
+Dinheiro Reserva::getValor() const {
+     return this->valor;
+}
+
+void Reserva::setCodigo(Codigo novoCodigo) {
+     this->codigo = novoCodigo;
+}
+
+Codigo Reserva::getCodigo() const {
+     return this->codigo;
+}
+
+
+// ----------  QUARTO  ---------- 
+
+void Quarto::setNumero(Numero novoNumero) {
+     this->numero = novoNumero;
+}
+
+Numero Quarto::getNumero() const {
+     return this->numero;
+}
+
+void Quarto::setCapacidade(Capacidade novaCapacidade) {
+     this->capacidade = novaCapacidade;
+}
+
+Capacidade Quarto::getCapacidade() const {
+     return this->capacidade;
+}
+
+void Quarto::setDiaria(Dinheiro novaDiaria) {
+     this->diaria = novaDiaria;
+}
+
+Dinheiro Quarto::getDiaria() const {
+     return this->diaria;
+}
+
+void Quarto::setRamal(Ramal novoRamal) {
+     this->ramal = novoRamal;
+}
+
+Ramal Quarto::getRamal() const {
+     return this->ramal;
+}
+
+
+// ----------  HOTEL ---------- 
+
+void Hotel::setNome(Nome novoNome) {
+     this->nome = novoNome;
+}
+
+Nome Hotel::getNome() const {
+     return this->nome;
+}
+
+void Hotel::setEndereco(Endereco novoEndereco) {
+     this->endereco = novoEndereco;
+}
+
+Endereco Hotel::getEndereco() const {
+     return this->endereco;
+}
+
+void Hotel::setTelefone(Telefone novoTelefone) {
+     this->telefone = novoTelefone;
+}
+
+Telefone Hotel::getTelefone() const {
+     return this->telefone;
+}
+
+void Hotel::setCodigo(Codigo novoCodigo) {
+     this->codigo = novoCodigo;
+}
+
+Codigo Hotel::getCodigo() const {
+     return this->codigo;
+}
--- entidades.hpp
+++ entidades.hpp
@@ -6,32 +6,90 @@
 using namespace std;
 
 class Pessoa {
-    public:
-        Nome nome;
-        Email email;
-};
-
+     private:
+         Nome nome;
+         Email email;
+     public:
+         void setNome(Nome);
+         Nome getNome() const;        
+         void setEmail(Email);
+         Email getEmail() const; 
+}
 class Gerente: public Pessoa {
-    private:
-        Senha senha; 
-    public: 
-        Ramal ramal;
-        Gerente(string, string);
+     private:
+         Senha senha; 
+     public: 
+         Ramal ramal;
+         void setSenha(Senha);
+         Senha getSenha() const;         
+         void setRamal(Ramal);
+         Ramal getRamal() const;    
 };
-
-// void Gerente::Gerente(string email, string senha) {
-//     int res1 = Email::setValor(email);
-//     int res2 = Senha::setValor(senha);
-//     int res3 = Ramal::setValor(ramal);
-// };
 
 class Hospede: public Pessoa {
-    private: 
-        Cartao cartao;
-    public:
-        Endereco endereco;
+     private: 
+         Cartao cartao;
+         Endereco endereco;
+     public:
+         void setCartao(Cartao);
+         Cartao getCartao() const;          
+         void setEndereco(Endereco);
+         Endereco getEndereco() const; 
+
 };
 
+class Reserva {
+     private:
+         Data chegada;
+         Data partida;
+         Dinheiro valor;
+         Codigo codigo; 
+     public:
+         void setChegada(Data);
+         Data getChegada() const;        
+         void setPartida(Data);
+         Data getPartida() const;        
+         void setValor(Dinheiro);
+         Dinheiro getValor() const;
+         void setCodigo(Codigo);
+         Codigo getCodigo() const; ;
+}
+class Quarto {
+     private:
+         Numero numero;
+         Capacidade capacidade;
+         Dinheiro diaria;
+         Ramal ramal;
+     public:
+         void setNumero(Numero);
+         Numero getNumero() const;
 
+         void setCapacidade(Capacidade);
+         Capacidade getCapacidade() const; 
 
+         void setDiaria(Dinheiro);
+         Dinheiro getDiaria() const; 
+
+         void setRamal(Ramal);
+         Ramal getRamal() const; 
+}
+
+class Hotel{
+     private:
+         Nome nome;
+         Endereco endereco;
+         Telefone telefone;
+         Codigo codigo;
+     public:
+         void setNome(Nome);
+         Nome getNome() const;        
+         void setEndereco(Endereco);
+         Endereco getEndereco() const; 
+
+         void setTelefone(Telefone);
+         Telefone getTelefone() const; 
+
+         void setCodigo(Codigo);
+         Codigo getCodigo() const; ;
+}
 #endif // ENTIDADES_HPP_INCLUDED
